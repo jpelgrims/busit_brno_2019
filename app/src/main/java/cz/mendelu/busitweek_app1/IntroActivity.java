336f5ad94dc05cae907ba7e1ffa8cb4f843683d8
Jelle Pelgrims <github@jellepelgrims.com>
2019-02-07 08:38:34 +0100

    Added explanation screen

---
+++ app/src/main/java/cz/mendelu/busitweek_app1/IntroActivity.java
@@ -0,0 +1,25 @@
+package cz.mendelu.busitweek_app1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void start(View view) {
+        Intent intent = new Intent(IntroActivity.this, MapActivity.class);
+        startActivity(intent);
+    }
+}
