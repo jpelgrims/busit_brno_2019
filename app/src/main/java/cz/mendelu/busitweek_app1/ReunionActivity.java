588db5c231628b1a63885865b0e62ef46d946b97
florian berden <florian.berden@student.ucll.be>
2019-02-07 15:12:38 +0100

    help decibel

--- app/src/main/java/cz/mendelu/busitweek_app1/ReunionActivity.java
+++ app/src/main/java/cz/mendelu/busitweek_app1/ReunionActivity.java
@@ -16,16 +16,6 @@ public class ReunionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reunion);
         addListenerOnButton();
-        /*
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
 
     }
 
