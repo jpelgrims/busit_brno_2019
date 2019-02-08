b28f73ab42793f5f8ab1d01a66080d8446d247b0
Jelle Pelgrims <github@jellepelgrims.com>
2019-02-08 09:50:39 +0100

    Radius

--- app/src/main/java/cz/mendelu/busitweek_app1/BusITWeekDatabaseHelper.java
+++ app/src/main/java/cz/mendelu/busitweek_app1/BusITWeekDatabaseHelper.java
@@ -6,7 +6,7 @@ import cz.mendelu.busItWeek.library.builder.StoryLineBuilder;
 public class BusITWeekDatabaseHelper extends StoryLineDatabaseHelper {
 
     public BusITWeekDatabaseHelper() {
-        super(114);
+        super(116);
     }
 
     @Override
@@ -14,31 +14,31 @@ public class BusITWeekDatabaseHelper extends StoryLineDatabaseHelper {
 
         // See cat
         builder.addGPSTask("1")
-                .radius(5)
+                .radius(10)
                 .location(49.210098, 16.61469)
                 .taskDone();
 
         // Catch or lose cat
         builder.addGPSTask("2")
                 .location(49.210425, 16.615061)
-                .radius(5)
+                .radius(10)
                 .taskDone();
 
         // PeeTask
         builder.addGPSTask("3")
                 .location(49.210775, 16.616693)
-                .radius(5)
+                .radius(10)
                 .taskDone();
 
         // DigTask
         builder.addGPSTask("4")
                 .location(49.209808, 16.616381)
-                .radius(5)
+                .radius(10)
                 .taskDone();
 
         builder.addGPSTask("5")
                 .location(49.209808, 16.616381)
-                .radius(5)
+                .radius(10)
                 .imageSelectPuzzle()
                 .addImage(R.drawable.banaanschil, true)
                 .addImage(R.drawable.bird, true)
@@ -51,7 +51,7 @@ public class BusITWeekDatabaseHelper extends StoryLineDatabaseHelper {
         //BarkTask
         builder.addGPSTask("6")
                 .location(49.209630, 16.615110)
-                .radius(5)
+                .radius(10)
                 .taskDone();
 
 
